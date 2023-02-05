e09e01dd3ca76ebf0c3630a8bf607c7abd1acd67
Uliana Le <ulianochka29@mail.ru>
2023-02-06 00:15:11 +0300

    задания готовы

--- 3.arrays/task.js
+++ 3.arrays/task.js
@@ -1,7 +1,15 @@
 function compareArrays(arr1, arr2) {
-  
+    return (arr1.length == arr2.length && arr1.every((element, index) => element == arr2[index])); 
 }
 
 function getUsersNamesInAgeRange(users, gender) {
-  
+    let arr = users.filter(i => i.gender === gender).map(i => i.age);
+    let res = arr.reduce((acc, item, index, arr) => {
+      acc += item;
+      if (index == arr.length - 1) {
+        return (acc / arr.length);
+      }
+      return acc;
+    }, 0);
+    return res;
 }
