430d167c7c9485753c8f27e53d030371fd23c169
Uliana Le <ulianochka29@mail.ru>
2023-02-13 04:07:09 +0300

    задание готово

--- 4.objects/task.js
+++ 4.objects/task.js
@@ -1,19 +1,37 @@
 function Student(name, gender, age) {
+    this.name = name;
+    this.gender = gender;
+    this.age = age;
+    this.marks = [];
   
-}
-
-Student.prototype.setSubject = function (subjectName) {
+  }
   
-}
-
-Student.prototype.addMarks = function (...marks) {
+  Student.prototype.setSubject = function(subjectName) {
+    this.subject = subjectName;
+  }
   
-}
-
-Student.prototype.getAverage = function () {
+  Student.prototype.addMarks = function(...marks) {
+    if (this.marks !== undefined) {
+      this.marks.push(...marks);
+    }
   
-}
-
-Student.prototype.exclude = function (reason) {
+  }
   
-}
+  Student.prototype.getAverage = function() {
+    let sum = 0;
+    if (this.marks === undefined || this.marks.length == 0) {
+      return 0;
+    } else {
+      this.marks.forEach(i => sum += i);
+      return sum / this.marks.length;
+    }
+  
+  }
+  
+  Student.prototype.exclude = function(reason) {
+    delete this.subject;
+    delete this.marks;
+    this.excluded = reason;
+  
+  }
+  
